cd3c7e1c6780465f787445e971028706f96d0f86
uhauha2929 <ck143302@163.com>
2022-03-27 20:42:33 +0800

    ...

--- stutils/string/common.py
+++ stutils/string/common.py
@@ -6,6 +6,30 @@ import string
 import re
 from typing import Callable
 
+special_pw_chars = '^$_@!%*#~?&'
+
+
+def rand_password(length: int) -> str:
+    """6-16位随机密码"""
+    if length < 6 or length > 16:
+        raise ValueError('Unsupported length: %d' % length)
+    password = []
+    chars_list = [string.ascii_lowercase, string.ascii_uppercase, string.digits, special_pw_chars]
+    remain_length = length
+    for i, group in enumerate(chars_list, start=1):
+        n = random.randint(1, remain_length - (len(chars_list) - i)) 
+        if i == len(chars_list):
+            n = remain_length
+        password.extend(random.choices(list(group), k=n))
+        remain_length -= n
+    random.shuffle(password)
+    return ''.join(password)
+
+
+def is_strict_password(password):
+    """是否是6-16位的严格密码"""
+    return re.match(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[\s\S]{6,16}$', password) is not None
+
 
 def rand_string(length: int, exclude='') -> str:
     """返回指定长度的随机字符串"""
